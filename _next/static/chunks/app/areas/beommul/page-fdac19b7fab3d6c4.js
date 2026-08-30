1abfd83b563e5ead41247409af6e59c409b8e670
ddok <sangjun2e@gmail.com>
2026-08-30 22:43:31 +0900

    deploy: restore preview build

---
+++ _next/static/chunks/app/areas/beommul/page-fdac19b7fab3d6c4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[158,220,230,282,410,487,550,614,822,849,957,965],{1895:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8500,23))}},e=>{e.O(0,[500,441,794,358],()=>e(e.s=1895)),_N_E=e.O()}]);
